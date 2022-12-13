73c0509049bb9a9a08461cf3a4cc09f9ff4cb47d
Drums180 <daviddomor180@hotmail.com>
2022-12-12 23:21:48 -0600

    Debug, not returning to correct value of lenght

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function writePassword() {
         Passlenght();return;
       } else if(isNaN(PasswordLenght)) { //Check if its a number
         alert("It is not a number. Please enter a number from 8 to 125")
+        Passlenght();return;
       } else {
         alert("Lenght accepted");
       }
